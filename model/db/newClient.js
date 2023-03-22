3ec363763a57b52ed631f4da9ca0e47163de3e24
DanNisenson <danpablo1@gmail.com>
2023-03-22 20:55:30 +0100

    👷test using aws sdk

--- model/db/newClient.js
+++ model/db/newClient.js
@@ -12,12 +12,32 @@ const dbConnect = async () => {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    password: awsSecret(),
     database: process.env.PG_DATABASE,
   };
   const newClient = new Client(connectionData);
   newClient.connect();
   return newClient;
 };
+
+const awsSecret = async () => {
+  const config = {
+    credentials: {
+      accessKeyId: process.env.DB_ACCESS_KEY_ID,
+      secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
+    },
+    region: 'eu-central-1a',
+  }
+
+  const input = {
+    SecretId: process.env.DB_SECRET_ID,
+  }
+
+  const client = new SecretsManagerClient(config)
+  const command = new GetSecretValueCommand(input)
+  const response = await client.send(command)
+
+  return response
+}
 
 module.exports = dbConnect;
